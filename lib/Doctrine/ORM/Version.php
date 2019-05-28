538a449e792ec68f09eebbccdfdb6e387e0b60fd
Damien Deconinck <deconinck.damien@gmail.com>
2019-05-28 15:32:48 +0200

    Bumping development version to v2.5.16-DEV

--- lib/Doctrine/ORM/Version.php
+++ lib/Doctrine/ORM/Version.php
@@ -36,7 +36,7 @@ class Version
     /**
      * Current Doctrine Version
      */
-    const VERSION = '2.5.15';
+    const VERSION = '2.5.16-DEV';
 
     /**
      * Compares a Doctrine version with the current one.
